22607ccc7e4548c184211c8aa8e6af755d48262e
Ashley Xu <ashxzq@gmail.com>
2022-07-09 23:40:07 -0700

    finish tab and feed page structure

--- PhotoRecipe/PhotoRecipe/Controller/FeedController.swift
+++ PhotoRecipe/PhotoRecipe/Controller/FeedController.swift
@@ -7,10 +7,41 @@
 
 import UIKit
 
-class FeedController: UIViewController {
+private let reuseIdentifier = "Cell"
+
+class FeedController: UICollectionViewController {
     
+    //MARK: - lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
+        configureUI()
+    }
+    
+    //MARK: - helper functions
+    func configureUI() {
+        collectionView.backgroundColor = .white
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
     }
 }
+    
+// MARK: - UICollectionViewDataSource
+extension FeedController {
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+extension FeedController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
+}
+
