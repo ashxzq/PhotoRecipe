f200764356e4c7ade603dce31d8932c72611217c
Ashley Xu <ashxzq@gmail.com>
2022-07-11 00:37:35 -0700

    finish Login page UI

---
+++ PhotoRecipe/PhotoRecipe/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,20 @@
+//
+//  RegistrationController.swift
+//  PhotoRecipe
+//
+//  Created by Ashley Xu on 7/10/22.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    // MARK: - Properties
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+        
+    }
+}
